e965838ac953fee652f6943f69b8204c8fc1e415
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-02-19 18:42:04 -0200

    Autenticacao

--- app/app.js
+++ app/app.js
@@ -10,6 +10,19 @@ angular.module('Pragueiro.services', []);
 
 angular.module('Pragueiro', ['Pragueiro.controllers', 'Pragueiro.config', 'Pragueiro.constant', 'Pragueiro.services']);
 
+angular.module('Pragueiro').run(['$rootScope', function($rootScope){
+
+	var ref = new Firebase("https://pragueiro.firebaseio.com");
+
+	$rootScope.logout = function(){
+		ref.unauth();
+		window.location.href = '/login';
+	};
+
+}]);
+
 angular.element(document).ready(function() {
+	var ref = new Firebase("https://pragueiro.firebaseio.com");
+	if(ref.getAuth() === null) window.location.href = '/login';
 	angular.bootstrap(document, ['Pragueiro']);
 });
