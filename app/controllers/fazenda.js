1104c92fe02b808a483edb129689316f9620f391
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-02-20 13:51:22 -0200

    Adicao safra

--- app/controllers/fazenda.js
+++ app/controllers/fazenda.js
@@ -4,9 +4,58 @@
 
 	angular.module('Pragueiro.controllers').registerCtrl('fazendaCtrl', fazendaCtrl);
 
-	fazendaCtrl.$inject = ['$scope'];
+	fazendaCtrl.$inject = ['$scope', '$firebaseArray', 'Constant', 'Session', 'Notify'];
 
-	function fazendaCtrl($scope) {
+	function fazendaCtrl($scope, $firebaseArray, Constant, Session, Notify) {
+
+		angular.extend($scope, {
+			edit: false,
+			fazendas: [],
+			data: {}
+		});
+
+		var ref = new Firebase(Constant.Url + '/filial');
+
+		$scope.fazendas = $firebaseArray(ref.orderByChild("key_usuario").equalTo(Session.getUser().uid));
+
+		$scope.salvarFazenda = function(data){
+			data.key_usuario = Session.getUser().uid;
+			$scope.fazendas.$add(data);
+			$scope.edit = false;
+			$scope.clear();
+			Notify.successBottom('Fazenda salva com sucesso!');
+		};
+
+		$scope.editarFazenda = function(data){
+			$scope.fazendas.$save(data);
+			$scope.edit = false;
+			$scope.clear();
+			Notify.successBottom('Fazenda atualizada com sucesso!');
+		};
+
+		$scope.cancelar = function(){
+			$scope.clear();
+			$scope.edit = false;
+		};
+
+		$scope.editar = function(data){
+			$scope.data = data;
+			$scope.edit = true;
+		};
+
+		$scope.clear = function(){
+			$scope.data = {};
+			angular.extend($scope.data, {
+				nome: '',
+				razaosocial: '',
+				telefone: '',
+				website: '',
+				cidade: '',
+				email: ''
+			});
+		};
+
+		$scope.clear();
 
   }
 
--- app/controllers/safra.js
+++ app/controllers/safra.js
@@ -4,10 +4,26 @@
 
 	angular.module('Pragueiro.controllers').registerCtrl('safraCtrl', safraCtrl);
 
-	safraCtrl.$inject = ['$scope'];
+	safraCtrl.$inject = ['$scope', 'Constant', 'Session', '$firebaseArray'];
 
-	function safraCtrl($scope) {
+	function safraCtrl($scope, Constant, Session, $firebaseArray) {
 
+		angular.extend($scope, {
+			fazendas: [],
+			data: {}
+		});
+
+		var ref = new Firebase(Constant.Url + '/filial');
+
+		$scope.fazendas = $firebaseArray(ref.orderByChild("key_usuario").equalTo(Session.getUser().uid));
+
+		$scope.clear = function(){
+			angular.extend($scope.data, {
+				fazenda: null
+			});
+		};
+
+		$scope.clear();
   }
 
 }());
