158afd123cd7aaa646310deac8344e81dc94e169
Fabricio Ronchi <fabricioronchii@gmail.com>
2016-09-23 09:38:00 -0300

    Files

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 'use strict';
 
 var express = require('express'),
-path = require('path'),
-bodyParser = require('body-parser'),
-firebase = require('./config/firebase'),
-app = express();
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	firebase = require('./config/firebase'),
+	fs = require('fs'),
+	app = express();
 
 app.use(express.static(path.join(__dirname, '/')));
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/arquivos', function(req, res, next) {
+	fs.readdir(__dirname + '/arquivos', function(err, data){
+	  if (err) {
+	  	res.send(err);
+	  }else{
+	  	res.send(data);
+	  }
+	});
+});
 
 app.get('/', function(req, res, next) {
 	res.sendfile(__dirname + '/views/index.html');
